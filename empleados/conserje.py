0c9f64e16420e9f86f27d21a6cb9c5d1fc0ad646
freixaaa <fiorella07freixa23.r@gmail.com>
2026-03-11 20:36:35 -0600

    Update conserje.py

--- empleados/conserje.py
+++ empleados/conserje.py
@@ -1,1 +1,10 @@
+from empleados.empleado import Empleado
 
+
+class Conserje(Empleado):
+
+    def __init__(self, nombre_persona, edad_persona):
+        super().__init__(nombre_persona, edad_persona)
+
+    def __str__(self):
+        return f"[Conserje] {self.informacion()}"
