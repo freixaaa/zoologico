e2b104f526ecd1082626fc893a3a7edf5e263aca
freixaaa <fiorella07freixa23.r@gmail.com>
2026-03-11 20:39:19 -0600

    Update transporte.py

--- transportes/transporte.py
+++ transportes/transporte.py
@@ -1,1 +1,18 @@
+class Transporte:
 
+    def __init__(self, marca_transporte):
+        self.__marca_transporte = marca_transporte
+
+    @property
+    def marca_transporte(self):
+        return self.__marca_transporte
+
+    @marca_transporte.setter
+    def marca_transporte(self, nueva_marca):
+        self.__marca_transporte = nueva_marca
+
+    def descripcion(self):
+        return f"Marca del transporte: {self.__marca_transporte}"
+
+    def __str__(self):
+        return self.descripcion()
