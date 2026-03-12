0351e9a944eaa537d332c7961f4417d5c60301b6
freixaaa <fiorella07freixa23.r@gmail.com>
2026-03-11 20:36:19 -0600

    Update administrador.py

--- empleados/administrador.py
+++ empleados/administrador.py
@@ -1,1 +1,10 @@
+from empleados.empleado import Empleado
 
+
+class Administrador(Empleado):
+
+    def __init__(self, nombre_persona, edad_persona):
+        super().__init__(nombre_persona, edad_persona)
+
+    def __str__(self):
+        return f"[Administrador] {self.informacion()}"
