49ea137d6fa9b4082b9fe5a2b6cdf749298c8f1d
freixaaa <fiorella07freixa23.r@gmail.com>
2026-03-11 20:34:34 -0600

    Update empleados.py

--- empleados/empleados.py
+++ empleados/empleados.py
@@ -1,1 +1,26 @@
+    def __init__(self, nombre_persona, edad_persona):
+        self.__nombre_persona = nombre_persona
+        self.__edad_persona = edad_persona
 
+    @property
+    def nombre_persona(self):
+        return self.__nombre_persona
+
+    @nombre_persona.setter
+    def nombre_persona(self, nuevo_nombre):
+        self.__nombre_persona = nuevo_nombre
+
+    @property
+    def edad_persona(self):
+        return self.__edad_persona
+
+    @edad_persona.setter
+    def edad_persona(self, nueva_edad):
+        self.__edad_persona = nueva_edad
+
+    def informacion(self):
+        return f"{self.__nombre_persona} ({self.__edad_persona} años)"
+
+    def __str__(self):
+        return self.informacion()
+      
