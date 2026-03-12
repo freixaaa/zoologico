eedf887f4ce79af0dad4c22a99da06905c56f3c3
freixaaa <fiorella07freixa23.r@gmail.com>
2026-03-11 20:48:05 -0600

    Update animal.py

--- animales/animal.py
+++ animales/animal.py
@@ -1,1 +1,18 @@
+class Animal:
 
+    def __init__(self, nombre_animal):
+        self.__nombre_animal = nombre_animal
+
+    @property
+    def nombre_animal(self):
+        return self.__nombre_animal
+
+    @nombre_animal.setter
+    def nombre_animal(self, nuevo_nombre):
+        self.__nombre_animal = nuevo_nombre
+
+    def datos(self):
+        return f"Animal registrado: {self.__nombre_animal}"
+
+    def __str__(self):
+        return self.datos()
